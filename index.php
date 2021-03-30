0b7490be746900432b95cdf2c2cb29070363e028
anjanettecastillo15 <adventureswithanjanette@gmail.com>
2021-03-30 21:17:56 +0800

    my first commit everything seems fine atm

---
+++ index.php
@@ -0,0 +1,353 @@
+<html>
+<head>
+    <title>Anj's Portfolio</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body data-spy="scroll" data-target="#navbarResponsive">
+
+<div id="home">
+  <!---NAVBAR START---->
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+    
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav mx-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#home">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#likes">Likes</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    
+  </nav>
+    <!---NAVBAR END---->
+</div>
+
+  <div class="background">
+    <!--BACKGROUND START-->
+      <img src="wjLY44B_.jpg" class="img-fluid">
+    <!--BACKGROUND END-->
+    </div>
+
+    <div class="heading-content text-center">
+      <!--HEADING START-->
+        <h5>Hello, I'm</h5>
+        <h1>Anjanette Castillo</h1>
+        <p>AND THIS IS MY WORLD</p>
+    <!--HEADING END-->
+      </div> 
+
+
+<!--ABOUT START-->
+<div id="about" class="offset container mt-3 mb-5">
+  <div class="post-heading text-center">
+    <h3 class="display-4 font-weight-bold">About Me</h3>
+    <hr class="w-50 mx-auto mb-5">
+  </div>
+  <div class="row">
+    <div class="col-lg-6 col-md-6 col-12">
+      <img src="117971389_1176079412762606_7526509498371256350_o.jpg" class="img-fluid">
+    </div>
+
+    <div class="col-lg-6 col-md-6 col-12">
+      <h2>Wanna know me?</h2>
+      <hr>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <a href="#" class="btn btn-dark w-50">KNOW MORE</a>
+    </div>
+  </div>
+</div>
+
+<!--ABOUT END-->
+
+<!--LIKES START-->
+<div id="likes" class="offset container mt-3 mb-5">
+<div class="post-heading text-center">
+    <h3 class="display-4 font-weight-bold">Likes</h3>
+    <hr class="w-50 mx-auto mb-5">
+  </div>
+
+  <div class="row">
+  <div class="col-lg-4 col-md-6 col-12 pb-5">
+  <div class="card">
+  <img src="354ae16c3efc7096dbb0ed214cbf71f8.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">BTS</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-dark">Preview</a>
+  </div>
+</div>
+  </div>
+
+  <div class="col-lg-4 col-md-6 col-12 pb-5">
+  <div class="card">
+  <img src="Space-clouds-Aesthetic-desktop-wallpaper-Desktop-.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Day Dreaming</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-dark">Preview</a>
+  </div>
+</div>
+  </div>
+
+  <div class="col-lg-4 col-md-6 col-12 pb-5">
+  <div class="card">
+  <img src="354ae16c3efc7096dbb0ed214cbf71f8.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">BTS</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-dark">Preview</a>
+  </div>
+</div>
+  </div>
+
+  <div class="col-lg-4 col-md-6 col-12 pb-5">
+  <div class="card">
+  <img src="Space-clouds-Aesthetic-desktop-wallpaper-Desktop-.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Day Dreaming</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-dark">Preview</a>
+  </div>
+</div>
+  </div>
+
+  <div class="col-lg-4 col-md-6 col-12 pb-5">
+  <div class="card">
+  <img src="354ae16c3efc7096dbb0ed214cbf71f8.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">BTS</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-dark">Preview</a>
+  </div>
+</div>
+  </div>
+
+  <div class="col-lg-4 col-md-6 col-12 pb-5">
+  <div class="card">
+  <img src="Space-clouds-Aesthetic-desktop-wallpaper-Desktop-.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Day Dreaming</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-dark">Preview</a>
+  </div>
+</div>
+  </div>
+  <a href="#" class="btn btn-dark w-50 mx-auto mb-5">KNOW MORE</a>
+</div>
+</div>
+
+<!--LIKES END-->
+
+<!--NEWSLETTER START-->
+<section class="sec1 container mt-5 pb-5">
+<h2 class="mt-5">Newsletter</h2>
+<p>Subscribe to my newsletter to receive my latest news.</p>
+<form class="form-inline mt-2" action="/action_page.php">
+  
+    <input type="email" class="form-control" placeholder="Enter email" id="email">
+ 
+  <button type="submit" class="btn btn-dark">Subscribe</button>
+</form>
+
+</section>
+<!--NEWSLETTER END-->
+
+<!--CONTACT START-->
+<div id="contact" class="offset w-50 mx-auto mt-5 mb-5">
+<div class="post-heading text-center">
+    <h3 class="display-4 font-weight-bold">Contact Me</h3>
+    <hr class="w-50 mx-auto mb-5">
+  </div>
+  <form action="/action_page.php">
+  <div class="form-group">
+    <label for="email">Full Name:</label>
+    <input type="text" class="form-control" id="fn">
+  </div>
+  <div class="form-group">
+    <label for="email">Email address:</label>
+    <input type="email" class="form-control" id="email">
+  </div>
+  <div class="form-group">
+    <label for="email">Your Message:</label>
+   <textarea class="form-control"></textarea>
+  </div>
+ 
+  <button type="submit" class="btn btn-dark">Send</button>
+</form>
+
+</div>
+
+<!--CONTACT END-->
+
+<!--FOOTER START-->
+<!-- Footer -->
+<div class="container-fluid">
+<footer class="page-footer font-small unique-color-dark">
+
+  <div style="background-color: #6351ce;">
+    <div class="container">
+
+      <!-- Grid row-->
+      <div class="row py-4 d-flex align-items-center">
+
+        <!-- Grid column -->
+        <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+          <h6 class="mb-0">Get connected with us on social networks!</h6>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-6 col-lg-7 text-center text-md-right">
+
+          <!-- Facebook -->
+          <a class="fb-ic">
+            <i class="fab fa-facebook-f white-text mr-4"> </i>
+          </a>
+          <!-- Twitter -->
+          <a class="tw-ic">
+            <i class="fab fa-twitter white-text mr-4"> </i>
+          </a>
+          <!-- Google +-->
+          <a class="gplus-ic">
+            <i class="fab fa-google-plus-g white-text mr-4"> </i>
+          </a>
+          <!--Linkedin -->
+          <a class="li-ic">
+            <i class="fab fa-linkedin-in white-text mr-4"> </i>
+          </a>
+          <!--Instagram-->
+          <a class="ins-ic">
+            <i class="fab fa-instagram white-text"> </i>
+          </a>
+
+        </div>
+        <!-- Grid column -->
+
+      </div>
+      <!-- Grid row-->
+
+    </div>
+  </div>
+
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left mt-5">
+
+    <!-- Grid row -->
+    <div class="row mt-3">
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+
+        <!-- Content -->
+        <h6 class="text-uppercase font-weight-bold">Company name</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
+          consectetur
+          adipisicing elit.</p>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+
+        <!-- Links -->
+        <h6 class="text-uppercase font-weight-bold">Products</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>
+          <a href="#!">MDBootstrap</a>
+        </p>
+        <p>
+          <a href="#!">MDWordPress</a>
+        </p>
+        <p>
+          <a href="#!">BrandFlow</a>
+        </p>
+        <p>
+          <a href="#!">Bootstrap Angular</a>
+        </p>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+
+        <!-- Links -->
+        <h6 class="text-uppercase font-weight-bold">Useful links</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>
+          <a href="#!">Your Account</a>
+        </p>
+        <p>
+          <a href="#!">Become an Affiliate</a>
+        </p>
+        <p>
+          <a href="#!">Shipping Rates</a>
+        </p>
+        <p>
+          <a href="#!">Help</a>
+        </p>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+
+        <!-- Links -->
+        <h6 class="text-uppercase font-weight-bold">Contact</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>
+          <i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
+        <p>
+          <i class="fas fa-envelope mr-3"></i> info@example.com</p>
+        <p>
+          <i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+        <p>
+          <i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2020 Copyright: Anjanette Castillo
+  </div>
+  <!-- Copyright -->
+
+</footer>
+</div>
+<!-- Footer -->
+<!--FOOTER END-->
+    
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+</body>
+</html>

7310ba337b4c6388df860f3a34e247cb0da32d6c
anjanettecastillo15 <adventureswithanjanette@gmail.com>
2021-03-31 21:35:21 +0800

    ayaw parin magpakita ng mga icons grrr

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="style.css">
-    <link href="C:\xampp\htdocs\portfolio\css\all.css" rel="stylesheet">
     
   </head>
 <body data-spy="scroll" data-target="#navbarResponsive">

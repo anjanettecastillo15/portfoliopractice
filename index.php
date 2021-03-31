af1c31f9d2e88fd98da13077bcc7140364190b80
anjanettecastillo15 <adventureswithanjanette@gmail.com>
2021-03-31 21:31:33 +0800

    ayaw parin magpakita ng mga icons grrr

--- index.php
+++ index.php
@@ -5,9 +5,11 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="style.css">
-</head>
+    <link href="C:\xampp\htdocs\portfolio\css\all.css" rel="stylesheet">
+    
+  </head>
 <body data-spy="scroll" data-target="#navbarResponsive">
 
 <div id="home">
